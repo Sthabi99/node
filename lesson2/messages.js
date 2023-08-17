c5d0844f57fe04759cb9fbe85fae05347b617322
sthabi99 <reggi9908@gmail.com>
2023-08-17 08:24:58 +0200

    npm install

---
+++ lesson2/messages.js
@@ -0,0 +1,15 @@
+// console.log("Hello, Universe!");
+
+// ("use strict");
+
+// let messages = [
+//   "A change of environment can be a good thing!",
+//   "You will make it!",
+//   "Just run with the code!",
+// ];
+
+exports.messages = [
+  "You are great!",
+  "You can accomplish anything!",
+  "Success is in your future! ",
+];
