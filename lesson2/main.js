318e922dfba8e0dc7bf42fa3b052d240c3f112ba
sthabi99 <reggi9908@gmail.com>
2023-08-17 08:28:33 +0200

    lesson2

--- lesson2/main.js
+++ lesson2/main.js
@@ -1,12 +1,3 @@
-// {
-//     "name": "recipe_connection",
-//     "version": "1.0.0",
-//     "description": "An app to share cooking recipes",
-//     "main": "main.js",
-//     "scripts": {
-//     "test": "echo \"Error: no test specified\" && exit 1"
-//     },
-//     "author": "Jon Wexler",
-//     "license": "ISC"
-//     }
-
+const cities = require("cities");
+var myCity = cities.zip_lookup("10016");
+console.log(myCity);
