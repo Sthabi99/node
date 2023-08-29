04043441b0082b947625eb093fe0ae9003da4633
sthabi99 <reggi9908@gmail.com>
2023-08-29 08:53:15 +0200

    lesson 9

--- lesson8/main.js
+++ lesson8/main.js
@@ -3,6 +3,10 @@ const port = 3000,
   app = express();
 app
   .get("/", (req, res) => {
+    console.log(req.params);
+    console.log(req.body);
+    console.log(req.url);
+    console.log(req.query);
     res.send("Hello, Universe!");
   })
   .listen(port, () => {
