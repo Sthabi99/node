8ed1e33a6afa54256214234dd7e7b53c8a376e56
sthabi99 <reggi9908@gmail.com>
2023-09-18 08:28:38 +0200

    lesson 10 commits

--- lesson10/express_templates/main.js
+++ lesson10/express_templates/main.js
@@ -1,6 +1,23 @@
 const port = 3000,
-express = require("express"),
-app = express();
-app.set("view engine", "ejs")
-respondWithName: app.get("/name", homeController.respondWithName)
-.log(`Server running at http://localhost:${ app.get("port") }`);
+  express = require("express"),
+  fs = require("fs"),
+  app = express();
+app.set("view engine", "ejs");
+respondWithName: app
+  .get("/name", homeController.respondWithName)
+  .log(`Server running at http://localhost:${app.get("port")}`);
+
+
+  const express = require("express");
+   app = express()
+   homeController = require("./controllers/homeController"),
+   layouts=require("express-ejs-layouts");
+
+   app.set("port", process.env.PORT||3000)
+   app.set("view engine","ejs");
+
+   app.use(layouts);
+   app.use( express.urlencoded({extended:false})
+
+   )
+   app.use(express.json());
