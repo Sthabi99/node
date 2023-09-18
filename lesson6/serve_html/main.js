8640f7f2a3fa3d77564f01983d162736d2c783a0
sthabi99 <reggi9908@gmail.com>
2023-09-18 05:44:42 +0200

    lesson 7

--- lesson6/serve_html/main.js
+++ lesson6/serve_html/main.js
@@ -5,20 +5,24 @@ const port = 3000,
 const routeMap = {
   "/": "views/index.html",
 };
+const getViewUrl = (url) => {
+  return `views${url}.html`;
+};
 http
   .createServer((req, res) => {
-    res.writeHead(httpStatus.OK, {
-      "Content-Type": "text/html",
-    });
-    if (routeMap[req.url]) {
-      fs.readFile(routeMap[req.url], (error, data) => {
+    let viewUrl = getViewUrl(req.url);
+    fs.readFile(viewUrl, (error, data) => {
+      if (error) {
+        res.writeHead(httpStatus.NOT_FOUND);
+        res.write("<h1>FILE NOT FOUND</h1>");
+      } else {
+        res.writeHead(httpStatus.OK, {
+          "Content-Type": "text/html",
+        });
         res.write(data);
-        res.end();
-      });
-    } else {
-      res.end("<h1>Sorry, not found.</h1>");
-    }
+      }
+      res.end();
+    });
   })
   .listen(port);
-console.log(`The server has started and is listening
-  ➥ on port number: ${port}`);
+console.log(`The server has started and is listening on port number: ${port}`);
