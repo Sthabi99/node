6e7f0c317782857696f370770a68025ffd2349bf
sthabi99 <reggi9908@gmail.com>
2023-09-18 07:10:42 +0200

    format

--- lesson7/confetti_cuisine/main.js
+++ lesson7/confetti_cuisine/main.js
@@ -1,7 +1,6 @@
-
-const port = 3300 ;
-http = require("http"),
-httpStatus = require("http-status-codes"),
-router = require("./router"),
-contentTypes = require("./contentTypes"),
-utils = require("./utils");
+const port = 3300;
+(http = require("http")),
+  (httpStatus = require("http-status-codes")),
+  (router = require("./router")),
+  (contentTypes = require("./contentTypes")),
+  (utils = require("./utils"));
